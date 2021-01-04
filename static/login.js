6ee1375346b8ce2ce0c82f29045605eb7d630a2d
Atichka <nosik_atichka@mail.ru>
2021-01-05 00:54:03 +0300

    fix

--- static/login.js
+++ static/login.js
@@ -1,7 +1,6 @@
 import { getDataForm } from "./script.js";
 
 const button = document.querySelector(".form__button");
-const form = document.forms.formlogin;
 button.addEventListener("click", (e) => {
   e.preventDefault();
   getDataForm(document.forms);
