46f42df08217e269e18003655098f299e3d3a940
Atichka <nosik_atichka@mail.ru>
2021-01-04 15:59:41 +0300

    add js file

--- static/login.js
+++ static/login.js
@@ -1,11 +1,22 @@
-const button = document.querySelector('.form__button');
+//const button = document.querySelector('.form__button');
 const form = document.forms.form;
+let obj = {};
 
-button.addEventListener('click', function() {
+// button.addEventListener('click', function() {
+//     let formData = new FormData(form);
+    
+//     for(let [name, value] of formData) {
+//         obj[name] = value;
+//     }
+//     console.log(obj);
+// })
+
+const button = document.querySelector('#submit');
+button.addEventListener('click', function(form) {
     let formData = new FormData(form);
-    let obj = {};
+    
     for(let [name, value] of formData) {
         obj[name] = value;
     }
     console.log(obj);
-})
+});
