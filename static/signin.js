77003433322d7b4a4c1c9955bea8c9e872b125f9
Atichka <nosik_atichka@mail.ru>
2021-01-05 01:17:25 +0300

    fix

---
+++ static/signin.js
@@ -0,0 +1,8 @@
+import { getDataForms } from "./script.js";
+
+const button = document.querySelector(".form__button");
+
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  getDataForms(document.forms);
+});
