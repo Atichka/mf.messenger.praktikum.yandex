c8b1737ccb0b639c0f2dfc5ee09b6ad052a94584
Atichka <nosik_atichka@mail.ru>
2021-01-07 13:48:17 +0300

    del file js

--- static/chats.js
+++
@@ -1,8 +0,0 @@
-// import { getDataForms } from "./script.js";
-
-// const button = document.querySelector(".message__button-send");
-
-// button.addEventListener("click", (e) => {
-//   e.preventDefault();
-//   getDataForms(document.forms);
-// });
